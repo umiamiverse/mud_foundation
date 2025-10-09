5aa183ced2466f69b3a9afde2294a9928b6ad87d
umiamiverse <120676662+umiamiverse@users.noreply.github.com>
2025-10-09 16:35:32 -0400

    Update CreatingMaterials.js

--- CreatingMaterials.js
+++ CreatingMaterials.js
@@ -9,3 +9,16 @@ function CreateSkyboxMaterial(imagePath){
     const curMaterial = new THREE.MeshBasicMaterial( { map:curTexture,side:THREE.BackSide });
     return curMaterial;
 }
+
+//Usage: Skybox Example
+
+let skyBoxImagePath = 'https://persistence.mud.foundation/files/2be978a8-23a5-446d-8513-d4c3b5e7c8f9.png';
+
+let skyboxMaterial = CreateMaterialFromTexture(skyBoxImagePath);
+
+let skyBoxObject3D = cast(self.parent, THREE.Object3D);
+
+function startup() {
+    skyBoxObject3D.children[0].material = skyboxMaterial;
+
+}
